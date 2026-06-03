fc2730c9007ed4afe9f177d08618286a93bd35bb
Sanat <sanatsanjay4@gmail.com>
2026-06-03 08:01:25 +0000

    Initial folder structure

---
+++ Week_1/Day_1/removeduplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        int i=1;
+        if(n==0) return 0;
+        for(int j=1; j<n; j++){
+            if(nums[j] != nums[j-1]){
+                nums[i]=nums[j];
+                i++;
+            }
+
+        }
+        return i;
+    }
+}
