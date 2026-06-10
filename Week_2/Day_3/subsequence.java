bebe4429f95a89c81019b194d2cd06526fa8c8ef
Sanat <sanatsanjay4@gmail.com>
2026-06-10 11:47:46 +0000

    Add implementations for strStr and isSubsequence methods; create README file

---
+++ Week_2/Day_3/subsequence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()>t.length()) return false;
+        int j=0;
+        for(int i=0;i<t.length() && j<s.length();i++){
+         
+            if(s.charAt(j)==t.charAt(i)) j++;
+        }
+         if(j==s.length()) return true;
+        return false;
+    }
+}
